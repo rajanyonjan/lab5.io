65e45d25a315238eb6507a8c47d3341af778aeb6
rajan yonjan <rajanyonjan7@gmail.com>
2024-02-16 08:11:25 +0545

    This is the third message

--- src/test/MyUnitTest.js
+++ src/test/MyUnitTest.js
@@ -1,21 +1,23 @@
-const CustomAdd = (a, b) => {
-    return a + b;
-}
+const CustomAdd = (a,b)=> {
+    return a+b;
+};
 
-const testCases = [
-    { input: [2, 3], expectedOutput: 6},
-    { input: [2, 3], expectedOutput: 6},
-    { input: [2, 3], expectedOutput: 6},
+const testCases=[
+    { input: [2,3], expectedOutput: 5},
+    { input: [0,0], expectedOutput: 0},
+    { input: [-1,4], expectedOutput: 3},
 ];
 
-testCases.forEach((testCase) => {
-    const result = CustomAdd(testCase.input[0], testCase.input[1]);
+//Run Tests
+testCases.forEach((testCases) => {
+    const result = CustomAdd(testCases.input[0], testCases.input[1]);
 
-    if(result === testCase.expectedOutput) {
-        console.log(Test passed: ${testCase.input} added correctly);
+    if(result === testCases.expectedOutput){
+        console.log(`Test passed: $(testCase.input) added correctly`);
     }else{
         console.error(
-            Test failed: Expected ${testCase.expectedOutput}, got ${result}
+            `Test failed: Expected $(testCase.expectedOutput),got $(result)`
         );
     }
+
 });
